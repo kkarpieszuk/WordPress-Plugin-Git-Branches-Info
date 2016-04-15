f48b87e76dada3f7c96e1216da3e07e94bbe3bc3
Konrad Karpieszuk <konrad.k@icanlocalize.com>
2016-04-15 10:56:58 +0200

    Changed time_elapsed_stringhuman_time_diff

--- git-branches-info.php
+++ git-branches-info.php
@@ -131,49 +131,10 @@ class Git_Branches_Client {
 	}
 
 	function file_time_string($git_fetch_head_file_time) {
-		$time_elapsed = $this->time_elapsed_string($git_fetch_head_file_time);
+		$time_elapsed = human_time_diff($git_fetch_head_file_time);
 		$date = date('l jS \of F Y h:i:s A', $git_fetch_head_file_time);
 		return "<span title='".$date."'>". __("pulled", "git-branch-plugin") . " " . $time_elapsed . "</span>";
 	}
-
-
-	// http://stackoverflow.com/questions/1416697/converting-timestamp-to-time-ago-in-php-e-g-1-day-ago-2-days-ago
-	function time_elapsed_string($ptime)
-	{
-	    $etime = time() - $ptime;
-
-	    if ($etime < 1)
-	    {
-	        return '0 seconds';
-	    }
-
-	    $a = array( 365 * 24 * 60 * 60  =>  'year',
-	                 30 * 24 * 60 * 60  =>  'month',
-	                      24 * 60 * 60  =>  'day',
-	                           60 * 60  =>  'hour',
-	                                60  =>  'minute',
-	                                 1  =>  'second'
-	                );
-	    $a_plural = array( 'year'   => 'years',
-	                       'month'  => 'months',
-	                       'day'    => 'days',
-	                       'hour'   => 'hours',
-	                       'minute' => 'minutes',
-	                       'second' => 'seconds'
-	                );
-
-	    foreach ($a as $secs => $str)
-	    {
-	        $d = $etime / $secs;
-	        if ($d >= 1)
-	        {
-	            $r = round($d);
-	            return $r . ' ' . ($r > 1 ? $a_plural[$str] : $str) . ' ago';
-	        }
-	    }
-	}
-
-
 
 }
 
